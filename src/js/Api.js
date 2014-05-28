e8dc6f1f08f5ff64ab22f1af8d4d888734612e89
Andrew Hobden <andrew@hoverbear.org>
2014-05-28 16:15:05 -0700

    Ensure tabs are used.

--- src/js/Api.js
+++ src/js/Api.js
@@ -1,17 +1,17 @@
 function Api() {
 	'use strict';
-    this.version = '/v1';
-    this.root = '/api';
+	this.version = '/v1';
+	this.root = '/api';
 
-    // Returns a string used to query the API.
-    this.queries = function (opts) {
-        var url = this.root + this.version + '/queries';
-        if (opts) {
-            url += '?';
-            url += (opts.limit)   ? 'limit='   + opts.limit   + '&' : '';
-            url += (opts.skip)    ? 'skip='    + opts.skip    + '&' : '';
-            url += (opts.search)  ? 'search='  + opts.search  + '&' : '';
-        }
-        return url;
-    };
+	// Returns a string used to query the API.
+	this.queries = function (opts) {
+		var url = this.root + this.version + '/queries';
+		if (opts) {
+			url += '?';
+			url += (opts.limit)	  ? 'limit='   + opts.limit	  + '&' : '';
+			url += (opts.skip)	  ? 'skip='	   + opts.skip	  + '&' : '';
+			url += (opts.search)  ? 'search='  + opts.search  + '&' : '';
+		}
+		return url;
+	};
 }
--- src/js/Sidebar.js
+++ src/js/Sidebar.js
@@ -1,24 +1,28 @@
 function Sidebar(api) {
 	'use strict';
-    // Populate the sidebar.
-    d3.json(api.queries(), this.populateQueries);
+	// Populate the sidebar.
+	this.populateQueries = function() {
+		d3.json(api.queries(), function (error, data) {
+			if (error) {
+				throw error;
+			}
 
-	this.populateQueries = function (error, data) {
-		if (error) {
-			throw error;
-		}
-	    var items = d3.select('#queries').selectAll('li').data(data),
-			limit = 25;
+			var items = d3.select('#queries').selectAll('li').data(data),
+				limit = 25;
 
-	    items.enter()
-	        .append('li')
-	        .append('a')
-	        .text(function (d) {
-	            if (d.title.length > limit) {
-	                return d.title.substr(0, limit) + "...";
-	            } else {
-	                return d.title;
-	            }
-	        });
+			items.enter()
+				.append('li')
+				.append('a')
+				.text(function (d) {
+					if (d.title.length > limit) {
+						return d.title.substr(0, limit) + "...";
+					} else {
+						return d.title;
+					}
+				});
+		});
 	}
+	
+	// Finish up
+	this.populateQueries();
 }
